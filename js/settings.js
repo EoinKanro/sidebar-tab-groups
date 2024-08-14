ec759fd56a792b3f3d7a9c03f846916f1ed44849
EoinKanro <ivanros.nh@gmail.com>
2024-08-14 19:03:40 +0400

    Add checks on save backup settings

--- js/settings.js
+++ js/settings.js
@@ -21,8 +21,17 @@ backupTime.addEventListener('input', (event) => {
 
 //Update on click
 saveBackup.addEventListener('click', async (event) => {
+    if (backupCheckbox.checked && (!backupTime.value || Number(backupTime.value) <= 0)) {
+        alert("Wrong value of minutes for backup");
+        return
+    }
+
     await saveEnableBackup(backupCheckbox.checked);
     await saveBackupMinutes(backupTime.value);
     notify(notifyBackgroundUpdateBackup, null);
-    alert("Backup has been updated successfully.");
+
+    const alertText = backupCheckbox.checked ?
+        `Backup will be saved in Downloads every ${backupTime.value} minutes` :
+        `Backup switched off`;
+    alert(alertText);
 })
