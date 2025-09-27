50c9d3f5b4848bacca092477d8985cc2f1baa6f3
EoinKanro <54404008+EoinKanro@users.noreply.github.com>
2025-09-28 00:42:50 +0200

    Add todo

--- js/background.js
+++ js/background.js
@@ -15,6 +15,8 @@ browser.storage.local.onChanged.addListener((changes, areaName) => {
 
 //save tab to current group when opened
 browser.tabs.onCreated.addListener(async (tab) => {
+    console.log(tab)
+    //TODO windowId
     if (currentGroup) {
         console.log(`Saving new tab to current group: ${JSON.stringify(currentGroup, null, 0)}`)
 
@@ -26,6 +28,7 @@ browser.tabs.onCreated.addListener(async (tab) => {
 
 //save tab if it was updated
 browser.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+    //TODO windowId
     if (currentGroup && changeInfo.status && changeInfo.status === "complete") {
         const tabToChange = currentGroup.tabs.filter(tabF => tabF.id === tab.id)[0];
         if (!tabToChange || tabToChange.url === tab.url) {
@@ -42,6 +45,7 @@ browser.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
 
 //remove tab from current group when closed
 browser.tabs.onRemoved.addListener(async (tabId, removeInfo) => {
+    //TODO windowId
     if (currentGroup) {
         console.log(`Deleting tab: ${tabId} from current group: ${JSON.stringify(currentGroup, null, 0)}`)
 
