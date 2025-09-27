373c59b4c9f046747c627e06b3005eb59352259b
EoinKanro <54404008+EoinKanro@users.noreply.github.com>
2025-09-28 00:42:51 +0200

    Create edit context menu

--- js/sidebar.js
+++ js/sidebar.js
@@ -3,7 +3,7 @@ import {
     saveGroup,
     getAllOpenedTabs,
     getGroup,
-    saveActiveGroup, deleteActiveGroup, deleteGroupToEdit, getAllGroups, saveWindowId
+    saveActiveGroup, deleteActiveGroup, deleteGroupToEdit, getAllGroups, saveWindowId, saveGroupToEdit, TabsGroup
 } from "./data/dataStorage.js";
 
 import {notifySidebarReloadGroups} from "./data/events.js";
@@ -17,12 +17,35 @@ await deleteGroupToEdit();
 await saveWindowId(await getLatestWindowId());
 await reloadGroups();
 
+const groupIdAttribute = "groupId";
+
+const contextMenu = document.getElementById('context-menu');
+
 //Open create group on click
-document.getElementById('create-group').addEventListener('click', () => {
-    browser.windows.create({
-        url: browser.runtime.getURL("editGroup.html"),
-        type: "popup"
-    })
+document.getElementById('create-group').addEventListener('click', async () => {
+    await deleteGroupToEdit()
+    openGroupEditor();
+});
+
+//Process clicks on context menu of groups buttons
+contextMenu.addEventListener('click', async (event) => {
+    console.log(event)
+    if (event.target.tagName === 'LI' && event.target.id === 'edit-button') {
+        const groupToEditId = contextMenu.getAttribute(groupIdAttribute);
+        const groupToEdit = await getGroup(Number(groupToEditId), true);
+        const tempGroup = new TabsGroup(groupToEdit.name, groupToEdit.icon)
+        tempGroup.id = groupToEdit.id;
+        await saveGroupToEdit(tempGroup);
+        openGroupEditor();
+    }
+    contextMenu.style.display = 'none';
+});
+
+//Hide context menu on click outside
+document.addEventListener('click', (event) => {
+    if (!event.target.classList.contains('button') && event.target.closest('#context-menu') === null) {
+        contextMenu.style.display = 'none';
+    }
 });
 
 //Reload groups in sidebar on any updates
@@ -63,6 +86,17 @@ async function createButton(group) {
     button.addEventListener('click', async () => {
         const groupToOpen = await getGroup(group.id, true);
         await openTabs(groupToOpen);
+    });
+
+    //open context menu
+    button.addEventListener('contextmenu', (event) => {
+        event.preventDefault();
+
+        // Position the custom menu at the cursor position
+        contextMenu.setAttribute(groupIdAttribute, group.id);
+        contextMenu.style.left = `${event.pageX}px`;
+        contextMenu.style.top = `${event.pageY}px`;
+        contextMenu.style.display = 'block';
     });
 
     //add button
@@ -139,4 +173,11 @@ async function getLatestWindowId() {
     } catch (error) {
         return null;
     }
+}
+
+function openGroupEditor() {
+    browser.windows.create({
+        url: browser.runtime.getURL("editGroup.html"),
+        type: "popup"
+    })
 }
