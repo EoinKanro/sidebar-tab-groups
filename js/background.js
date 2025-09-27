ef1ad938af39168535a82600790db282b3c9699f
EoinKanro <54404008+EoinKanro@users.noreply.github.com>
2025-09-28 00:42:50 +0200

    Minor

--- js/background.js
+++ js/background.js
@@ -15,21 +15,17 @@ browser.storage.local.onChanged.addListener((changes, areaName) => {
 
 //save tab to current group when opened
 browser.tabs.onCreated.addListener(async (tab) => {
-    console.log(tab)
-    //TODO windowId
-    if (currentGroup) {
+    if (isAvailableToUpdate()) {
         console.log(`Saving new tab to current group: ${JSON.stringify(currentGroup, null, 0)}`)
 
         currentGroup.tabs.push(new Tab(tab.id, tab.url));
-        await saveCurrentGroup(currentGroup)
-        await saveGroup(currentGroup)
+        await save()
     }
 });
 
 //save tab if it was updated
 browser.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-    //TODO windowId
-    if (currentGroup && changeInfo.status && changeInfo.status === "complete") {
+    if (isAvailableToUpdate() && changeInfo.status && changeInfo.status === "complete") {
         const tabToChange = currentGroup.tabs.filter(tabF => tabF.id === tab.id)[0];
         if (!tabToChange || tabToChange.url === tab.url) {
             return
@@ -38,19 +34,26 @@ browser.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
         console.log(`Updating tab info: ${JSON.stringify(tab, null, 0)} in current group: ${JSON.stringify(currentGroup, null, 0)}`);
         tabToChange.url = tab.url;
 
-        await saveCurrentGroup(currentGroup)
-        await saveGroup(currentGroup)
+        await save()
     }
 });
 
 //remove tab from current group when closed
 browser.tabs.onRemoved.addListener(async (tabId, removeInfo) => {
-    //TODO windowId
-    if (currentGroup) {
+    if (isAvailableToUpdate()) {
         console.log(`Deleting tab: ${tabId} from current group: ${JSON.stringify(currentGroup, null, 0)}`)
 
         currentGroup.tabs = currentGroup.tabs.filter((tab) => tab.id !== tabId);
-        await saveCurrentGroup(currentGroup)
-        await saveGroup(currentGroup)
+        await save()
     }
 });
+
+function isAvailableToUpdate() {
+    //TODO windowId
+    return currentGroup
+}
+
+async function save() {
+    await saveCurrentGroup(currentGroup)
+    await saveGroup(currentGroup)
+}
